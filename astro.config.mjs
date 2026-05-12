d4ae89399c4552a6ec16e25828917265f92daf93
ayus79 <ayush@simprosys.com>
2026-05-12 12:44:25 +0530

    Update Astro configuration site URL and remove GitHub Pages deployment workflow.
    - Changed site URL to 'https://notiq.ayushcon2000.workers.dev'.
    - Removed the outdated GitHub Pages deployment workflow file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ayus79.github.io',
-  base: '/notiq-website',
+  site: 'https://notiq.ayushcon2000.workers.dev',
+  // base: '/notiq-website',
   integrations: [tailwind()],
   output: 'static',
 });
